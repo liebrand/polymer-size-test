bf505b4fde7e37e93e8257c23cc03294a9c154de
liebrand <jelte@liebrand.co.uk>
2016-10-15 08:40:17 -0700

    test of polymer size

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var vulcanize = require('gulp-vulcanize');
+var crisper = require('gulp-crisper');
+
+gulp.task('vulcanize', function() {
+  return gulp.src('elements/elements.html')
+    .pipe(vulcanize())
+    .pipe(crisper())
+    .pipe(gulp.dest('dist/elements'));
+  });
+
+gulp.task('default', ['vulcanize']);
